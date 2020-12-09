2c41547bec8d1b9e1bd1034a8a064db9407a132c
mudkip-hacker <liuenting990305@gmail.com>
2020-12-09 02:21:18 -0800

    Day 9

--- 2020-9.cpp
+++ 2020-9.cpp
@@ -8,82 +8,39 @@
 #include <set>
 using namespace std;
 
-bool contains(map<string, map<string, int>> m, string containingBag, string findBag, vector<string> searched)
-{
-    bool a = false;
-    // cout << "find " << findBag << " in " << containingBag << endl;
-    if (find(searched.begin(), searched.end(), containingBag) != searched.end())
-        return false;
-    searched.push_back(containingBag);
-    for (auto i = m[containingBag].begin(); i != m[containingBag].end(); i++)
-    {
-        if ((*i).first == findBag)
-            a = true;
-    }
-    bool b = false;
-    for (auto i = m[containingBag].begin(); i != m[containingBag].end(); i++)
-    {
-        if (contains(m, (*i).first, findBag, searched))
-            b = true;
-    }
-    return a || b;
-}
-
-int count(map<string, map<string, int>> m, string containingBag)
-{
-    int ct = 0;
-    // cout << "looking in " << containingBag << endl;
-    for (auto i = m.begin(); i != m.end(); i++)
-    {
-        if ((*i).first == containingBag)
-        {
-            if ((*i).second.size() == 0)
-            {
-                // cout << "terminal" << endl;
-                return 1;
-            }
-            for (auto j = (*i).second.begin(); j != (*i).second.end(); j++)
-            {
-                // cout << "in " << containingBag << " has " << (*j).second << (*j).first << endl;
-                int asd = (*j).second * count(m, (*j).first);
-                ct += asd;
-                // cout << "counted " << asd << " bags inside ct now " << ct << endl;
-            }
-        }
-    }
-    // cout << "returning" << ct << endl;
-    return ct + 1;
-}
-
 int main()
 {
     ifstream f("input.txt");
 
-    vector<int> xx;
+    vector<long long> xx;
     string foo;
     while (getline(f, foo))
     {
-        xx.push_back(stoi(foo));
+        xx.push_back(stoll(foo));
     }
     int x = 0;
-    int b = 5;
-    for (size_t j = b; j < xx.size(); j++)
+    int b = 25;
+    int ind = 0;
+    for (size_t i = b; i < xx.size(); i++)
     {
-        x = xx[j];
+        x = xx[i];
+        ind = i;
         bool found = false;
-        for (size_t i = 0; i < xx.size(); i++)
+        for (size_t j = i - b; j < i; j++)
         {
-            for (size_t k = 0; k < xx.size(); k++)
+            for (size_t k = i - b + 1; k < i; k++)
             {
-                printf("check %d + %d = %s against %d", xx[i], xx[k], xx[i] + xx[k], x);
-                if (xx[i] + xx[k] == x)
+                if (k == j)
+                    continue;
+                // printf("check %d %d    %d + %d = %d against %d\n", j, k, xx[j], xx[k], xx[j] + xx[k], x);
+                if (xx[j] + xx[k] == x)
                 {
                     found = true;
                     break;
                 }
-                if (found)
-                    break;
             }
+            if (found)
+                break;
         }
         if (!found)
         {
@@ -91,7 +48,36 @@ int main()
         }
     }
 
-    cout << x << endl;
+    //25
+
+    cout << x << " " << ind << endl;
+
+    int low = 0, high = 0;
+    for (low; low < ind; low++)
+    {
+        high = 0;
+        bool found = false;
+        long long acc = xx[low];
+        // cout << "start from " << acc << endl;
+        for (high = low + 1; high < ind; high++)
+        {
+            acc += xx[high];
+            // cout << "now is " << acc << endl;
+            if (acc == x)
+            {
+                // cout << "found " << acc << endl;
+                found = true;
+                break;
+            }
+        }
+        if (found)
+            break;
+    }
+
+    long long m = *min_element(xx.begin() + low, xx.begin() + high);
+    long long ma = *max_element(xx.begin() + low, xx.begin() + high);
+
+    cout << m << " " << ma << " " << m + ma << endl;
 
     return 0;
 }
